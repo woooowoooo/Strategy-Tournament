8eede6e37fc0fc93d288c4047cc9d0d98f3e52de
Ryan <ryan.zhang@digipen.edu>
2020-08-01 23:04:26 -0700

    Implemented strategies
    Still need to fill in the placeholder of "1" for last.

--- src/code/Strategy.java
+++ src/code/Strategy.java
@@ -10,6 +10,41 @@ public class Strategy {
 	public void setName(String newName) {
 		this.name = newName;
 	}
+	private int greederous(int[][][] data, boolean greedy) {
+		int diff = (data[0][0][0] + data[0][1][0]) - (data[1][0][0] + data[1][1][0]);
+		if (diff == 0) {
+			return random();
+		} else if ((diff < 0) ^ greedy) {
+			return 0;
+		} else {
+			return 1;
+		}
+	}
+	private int random() {
+		return (int) Math.round(Math.random());
+	}
+	private int minimize(int[][][] data) {
+		int diff = (data[0][0][1] + data[0][1][1]) - (data[1][0][1] + data[1][1][1]);
+		if (diff == 0) {
+			return random();
+		} else if (diff < 0) {
+			return 0;
+		} else {
+			return 1;
+		}
+	}
+	private int copycat(int last) {
+		return last;
+	}
+	private int beatlast(int[][][] data, int last) {
+		if (data[0][last][0] == data[1][last][0]) {
+			return random();
+		} else if (data[0][last][0] < data[1][last][0]) {
+			return 0;
+		} else {
+			return 1;
+		}
+	}
 	public int run(int[][][] data) {
 		switch (this.name) {
 			case "AlwaysA":
@@ -17,30 +52,24 @@ public class Strategy {
 			case "AlwaysB":
 				return 1;
 			case "Greedy":
-				if (data[0][0][0] + data[0][1][0] >= data[1][0][0] + data[1][1][0]) {
-					return 0;
-				} else {
-					return 1;
-				}
+				return greederous(data, true);
 			case "Generous":
-				if (data[0][0][0] + data[0][1][0] <= data[1][0][0] + data[1][1][0]) {
-					return 0;
-				} else {
-					return 1;
-				}
+				return greederous(data, false);
 			case "Random":
-				return (int) Math.round(Math.random());
+				return random();
 			case "Minimize":
-				if (data[0][0][1] + data[0][1][1] <= data[1][0][1] + data[1][1][1]) {
-					return 0;
-				} else {
-					return 1;
-				}
+				return minimize(data);
 			case "Copycat":
+				return copycat(1);
 			case "Beatlast":
+				return beatlast(data, 1);
 			default:
-				return 0;
+				System.err.println("Not a valid strategy, doing \"Random\".");
+				return random();
 		}
+	}
+	public String toString() {
+		return name;
 	}
 	public static void main(String[] args) {
 		Strategy strat = new Strategy("Greedy");
